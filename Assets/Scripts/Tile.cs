4bfd032a034f8a91ecfb2894702234ace0ca28cc
snipZ09 <105291682+snipZ09@users.noreply.github.com>
2022-05-30 18:09:50 +0700

    update boss hao`

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,15 +7,47 @@ using System;
 public class Tile : MonoBehaviour
 {
     public static Tile instance;
-    public int amount;
-    public Text amountText;
+    public int value
+    {
+        set
+        {
+            valueText.text = value.ToString();
+            Debug.Log("value:" + value);
+        }
+    }
+    public Text valueText;
     public float moveSpeed;
     public LayerMask whatStopsMovement;
-    bool canMove;
     public int xOffset, yOffset;
     public int stepCanMove;
-    bool isHit;
     int xMoveTo, yMoveTo;
+    public int order;
+
+    public Vector2 currentPos;
+    public TileManager tManager;
+
+
+    private void TestMoveRight()
+    {
+        //lay index thang ben phai cai nay
+        //neu no o ngoai cung thi thoi
+        Vector2 newPos = currentPos;
+        int columnCount = 4;
+        newPos.x = currentPos.x + 1 > columnCount ? columnCount : currentPos.x + 1;
+        //neu vi tri moi nam ngoai mang thi bo qua
+        if (newPos.x >= TileManager.instance.tiles.GetLength(0) || newPos.y >= TileManager.instance.tiles.GetLength(1))
+        {
+            Debug.Log("loi logic tim vi tri moi");
+            return;
+        }
+
+        Tile goInTargetCell = TileManager.instance.tiles[(int)newPos.x, (int)newPos.y];
+
+        //neu co object thi tinh, neu khong co thi move luon
+
+
+
+    }
 
 
     private void Awake()
@@ -23,92 +55,168 @@ public class Tile : MonoBehaviour
         instance = this;
     }
 
-    private void Start()
-    {
-        xMoveTo = CellManager.instance.maxX;
-        yMoveTo = CellManager.instance.maxY;
-    }
+
 
     private void Update()
     {
         //Tìm tile đang ở hàng nào
-        yOffset = gameObject.GetComponentInParent<Cell>().yIndex;
+        // currentPos.y = (float)this.GetComponentInParent<Cell>().yIndex;
         //Tìm tile đang ở cột nào
-        xOffset = gameObject.GetComponentInParent<Cell>().xIndex;
+        // currentPos.x = (float)this.GetComponentInParent<Cell>().xIndex;
+        //Debug.Log(currentPos.x + " - " + currentPos.y);
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            MoveRight(yOffset);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            MoveLeft(yOffset);
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            MoveUp(xOffset);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            MoveDown(xOffset);
-        }
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    MoveRight(yOffset);
+        //}
+
+        //valueText.text = "" + value;
     }
 
     public void MoveRight(int row)
     {
-        this.xMoveTo = CellManager.instance.maxX;
-
-        while(transform.position != CellManager.instance.cells[row, xMoveTo].transform.position)
+        this.xMoveTo = TileManager.instance.maxX;
+        //Di chuyển nhưng chưa có nhập lại được
+        while (transform.position != TileManager.instance.tiles[row, xMoveTo].transform.position)
         {
-            if(CellManager.instance.cells[row, xMoveTo].GetComponent<Cell>().hasTile)
+            if (TileManager.instance.tiles[row, xMoveTo].GetComponent<Cell>().hasTile)
             {
-                if (xMoveTo == CellManager.instance.minX)
+                if (xMoveTo == TileManager.instance.minX)
                 {
                     return;
                 }
-                xMoveTo--;  
+                xMoveTo--;
             }
             else
             {
-                transform.position = CellManager.instance.cells[row, xMoveTo].transform.position;
-                transform.parent = CellManager.instance.cells[row, xMoveTo].transform;
+                transform.position = TileManager.instance.tiles[row, xMoveTo].transform.position;
+                transform.parent = TileManager.instance.tiles[row, xMoveTo].transform;
             }
-            
         }
     }
 
-    public void MoveLeft(int row)
+    public void MoveLeft()
     {
-        this.xMoveTo = CellManager.instance.minX;
-
-        while (transform.position != CellManager.instance.cells[row, xMoveTo].transform.position)
+        Debug.Log("Move left");
+        Vector2 newPos = GetNewPos(3);
+        Debug.Log("new pos:" + newPos);
+        Debug.Log("currentPos:" + currentPos);
+        //neu vi tri moi nam ngoai mang thi bo qua
+        if (newPos.x >= TileManager.instance.tiles.GetLength(0) || newPos.y >= TileManager.instance.tiles.GetLength(1))
         {
-            if (CellManager.instance.cells[row, xMoveTo].GetComponent<Cell>().hasTile)
-            {
-                if (xMoveTo == CellManager.instance.maxX)
-                {
-                    return;
-                }
-                xMoveTo++;
-            }
-            else
-            {
-                transform.position = CellManager.instance.cells[row, xMoveTo].transform.position;
-                transform.parent = CellManager.instance.cells[row, xMoveTo].transform;
-            }
+            Debug.Log("loi logic tim vi tri moi");
+            return;
+        }
 
+        ProcessMove(newPos);
+    }
+
+    public void MoveRight()
+    {
+        Debug.Log("Move right");
+        Vector2 newPos = GetNewPos(4);
+        Debug.Log("new pos:" + newPos);
+        Debug.Log("currentPos:" + currentPos);
+        //neu vi tri moi nam ngoai mang thi bo qua
+        if (newPos.x == -1 || newPos.y == -1 || newPos.x >= TileManager.instance.tiles.GetLength(0) || newPos.y >= TileManager.instance.tiles.GetLength(1))
+        {
+            Debug.Log("loi logic tim vi tri moi");
+            return;
+        }
+        ProcessMove(newPos);
+
+
+    }
+    public void MoveUp()
+    {
+        Debug.Log("Move up");
+        Vector2 newPos = GetNewPos(1);
+        Debug.Log("new pos:" + newPos);
+        Debug.Log("currentPos:" + currentPos);
+        //neu vi tri moi nam ngoai mang thi bo qua
+        if (newPos.x == -1 || newPos.y == -1 || newPos.x >= TileManager.instance.tiles.GetLength(0) || newPos.y >= TileManager.instance.tiles.GetLength(1))
+        {
+            Debug.Log("loi logic tim vi tri moi");
+            return;
+        }
+        ProcessMove(newPos);
+
+
+    }
+    public void MoveDown()
+    {
+        Debug.Log("Move up");
+        Vector2 newPos = GetNewPos(2);
+        Debug.Log("new pos:" + newPos);
+        Debug.Log("currentPos:" + currentPos);
+        //neu vi tri moi nam ngoai mang thi bo qua
+        if (newPos.x == -1 || newPos.y == -1 || newPos.x >= TileManager.instance.tiles.GetLength(0) || newPos.y >= TileManager.instance.tiles.GetLength(1))
+        {
+            Debug.Log("loi logic tim vi tri moi");
+            return;
+        }
+        ProcessMove(newPos);
+
+
+    }
+    //1: up 2:down 3:left 4:right
+    private Vector2 GetNewPos(int direction)
+    {
+        Vector2 newPos = currentPos;
+        switch (direction)
+        {
+            case 1:
+                newPos.y = currentPos.y;
+                newPos.x = currentPos.x - 1 < 0 ? 0 : currentPos.x - 1;
+                return newPos;
+
+            case 2:
+                newPos.y = currentPos.y;
+                newPos.x = currentPos.x + 1 > tManager.rowCount ? tManager.rowCount : currentPos.x + 1;
+                return newPos;
+            case 3:
+                newPos.x = currentPos.x;
+                newPos.y = currentPos.y - 1 < 0 ? 0 : currentPos.y - 1;
+                return newPos;
+            case 4:
+                newPos.x = currentPos.x;
+                newPos.y = currentPos.y + 1 > tManager.columnCount ? tManager.columnCount : currentPos.y + 1;
+                return newPos;
+
+            default:
+                return Vector2.one * -1;
         }
     }
+    private void ProcessMove(Vector2 newPos)
+    {
+        Tile goInTargetCell = tManager.tiles[(int)newPos.x, (int)newPos.y];
+        if (goInTargetCell == null)
+        {
+            Debug.Log("goInTargetCell ==null");
+
+            tManager.tiles[(int)newPos.x, (int)newPos.y] = this;
+            tManager.tiles[(int)currentPos.x, (int)currentPos.y] = null;
+            transform.localPosition = tManager.positions[(int)newPos.x, (int)newPos.y];
+        }
+        else
+        {
+            Debug.Log("goInTargetCell !=null");
+            //TODO: i am here
+        }
+        currentPos = newPos;
+    }
+
+
 
     public void MoveUp(int column)
     {
-        this.yMoveTo = CellManager.instance.minY;
+        this.yMoveTo = TileManager.instance.minY;
 
-        while (transform.position != CellManager.instance.cells[yMoveTo, column].transform.position)
+        while (transform.position != TileManager.instance.tiles[yMoveTo, column].transform.position)
         {
-            if (CellManager.instance.cells[yMoveTo, column].GetComponent<Cell>().hasTile)
+            if (TileManager.instance.tiles[yMoveTo, column].GetComponent<Cell>().hasTile)
             {
-                if (yMoveTo == CellManager.instance.maxY)
+                if (yMoveTo == TileManager.instance.maxY)
                 {
                     return;
                 }
@@ -116,8 +224,8 @@ public class Tile : MonoBehaviour
             }
             else
             {
-                transform.position = CellManager.instance.cells[yMoveTo, column].transform.position;
-                transform.parent = CellManager.instance.cells[yMoveTo, column].transform;
+                transform.position = TileManager.instance.tiles[yMoveTo, column].transform.position;
+                transform.parent = TileManager.instance.tiles[yMoveTo, column].transform;
             }
 
         }
@@ -125,13 +233,13 @@ public class Tile : MonoBehaviour
 
     public void MoveDown(int column)
     {
-        this.yMoveTo = CellManager.instance.maxY;
+        this.yMoveTo = TileManager.instance.maxY;
 
-        while (transform.position != CellManager.instance.cells[yMoveTo, column].transform.position)
+        while (transform.position != TileManager.instance.tiles[yMoveTo, column].transform.position)
         {
-            if (CellManager.instance.cells[yMoveTo, column].GetComponent<Cell>().hasTile)
+            if (TileManager.instance.tiles[yMoveTo, column].GetComponent<Cell>().hasTile)
             {
-                if (yMoveTo == CellManager.instance.minY)
+                if (yMoveTo == TileManager.instance.minY)
                 {
                     return;
                 }
@@ -139,8 +247,8 @@ public class Tile : MonoBehaviour
             }
             else
             {
-                transform.position = CellManager.instance.cells[yMoveTo, column].transform.position;
-                transform.parent = CellManager.instance.cells[yMoveTo, column].transform;
+                transform.position = TileManager.instance.tiles[yMoveTo, column].transform.position;
+                transform.parent = TileManager.instance.tiles[yMoveTo, column].transform;
             }
 
         }
@@ -148,12 +256,6 @@ public class Tile : MonoBehaviour
 
 
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Tile"))
-        {
-            /*if(amount == other.gameObject.)*/
-        }
-    }
-    
+
+
 }
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,242 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TileManager : MonoBehaviour
+{
+    public static TileManager instance;
+    public Cell[] allCells;
+    public Tile[] allTiles = new Tile[16];
+    public Tile[,] tiles = new Tile[4, 4];
+    public Tile tilePrefab;
+
+    public Vector2[,] positions = new Vector2[4, 4];
+
+    public int minX = 0, maxX = 3, minY = 0, maxY = 3;
+
+    public int columnCount = 4;
+    public int rowCount = 4;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    //tao mang vector2 tu 1 danh sach vi tri
+    private void CreatePostion()
+    {
+        for (int i = 0; i < allCells.Length; i++)
+        {
+            int firstDimensionIndex = i / 4;
+            int secondDimensionIndex = i % columnCount;
+
+            // Debug.Log("secondDimensionIndex:" + secondDimensionIndex);
+            positions[firstDimensionIndex, secondDimensionIndex] = allCells[i].transform.localPosition;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreatePostion();
+        //Test hàm tạo tile ở vị trí vector2
+        CreateTile(new Vector2(1, 1));
+        //Test hàm xóa tile ở vị trí vector2
+        //DeleteTile(new Vector2(1, 0));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddIndexToCell();
+#if UNITY_EDITOR
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            SpawnTileRandom();
+            //GameObject myNewTile = Instantiate(tileToSpawn, mang[1, 1].transform);
+        }
+#endif
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            CallMoveRight();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            CallMoveLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            CallMoveUp();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            CallMoveDown();
+        }
+
+    }
+
+    private void CallMoveRight()
+    {
+        for (int i = 0; i < tiles.GetLength(0); i++)
+        {
+            for (int j = tiles.GetLength(1) - 1; j >= 0; j--)
+            {
+                Tile tile = tiles[i, j];
+                if (tile != null)
+                {
+                    Debug.Log("call move right: " + i + "||" + j);
+                    tile.MoveRight();
+                }
+            }
+        }
+    }
+
+    private void CallMoveLeft()
+    {
+        for (int i = 0; i < tiles.GetLength(0); i++)
+        {
+            for (int j = 0; j < tiles.GetLength(1); j++)
+            {
+                Tile tile = tiles[i, j];
+                if (tile != null)
+                {
+                    Debug.Log("call move left");
+                    tile.MoveLeft();
+                }
+            }
+        }
+    }
+
+    private void CallMoveUp()
+    {
+        for (int i = 0; i < tiles.GetLength(0); i++)
+        {
+            for (int j = 0; j < tiles.GetLength(1); j++)
+            {
+                Tile tile = tiles[i, j];
+                if (tile != null)
+                {
+                    Debug.Log("call move left");
+                    tile.MoveUp();
+                }
+            }
+        }
+    }
+
+    private void CallMoveDown()
+    {
+        for (int i = tiles.GetLength(0) - 1; i >= 0; i--)
+        {
+            for (int j = 0; j < tiles.GetLength(1); j++)
+            {
+                Tile tile = tiles[i, j];
+                if (tile != null)
+                {
+                    Debug.Log("call move left");
+                    tile.MoveDown();
+                }
+            }
+        }
+    }
+
+    public void SpawnTileRandom()
+    {
+
+        var randomChange = Random.Range(0f, 1f);
+        var randomCell = Random.Range(0, allCells.Length);
+        if (allCells[randomCell].transform.childCount != 0)
+        {
+            Debug.Log("Cell " + randomCell + " has tile");
+            SpawnTileRandom();
+            return;
+        }
+        Tile myNewTile = Instantiate(tilePrefab, transform);
+        if (randomChange < 0.85f)
+        {
+            myNewTile.value = 2;
+        }
+        else
+        {
+            myNewTile.value = 4;
+        }
+    }
+
+    public void CreateTile(Vector2 pos)
+    {
+        //int count = CountIndex(pos);
+        Tile myNewTile = Instantiate(tilePrefab, transform);
+        //allTiles[count] = myNewTile;
+        myNewTile.transform.localPosition = positions[(int)pos.x, (int)pos.y];
+        myNewTile.tManager = this;
+        myNewTile.currentPos = pos;
+        myNewTile.value = 2;
+        tiles[(int)pos.x, (int)pos.y] = myNewTile;
+    }
+
+    public void DeleteTile(Vector2 pos)
+    {
+        int count = CountIndex(pos);
+        Debug.Log("Đã xóa tile ở vị trí x: " + pos.x + " y: " + pos.y);
+        Destroy(allTiles[count].gameObject);
+        allTiles[count] = null;
+    }
+
+    public int CountIndex(Vector2 pos)
+    {
+        int count = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                if (i == (int)pos.x && j == (int)pos.y)
+                {
+                    return count;
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void AddIndexToCell()
+    {
+        int count = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                allCells[count].yIndex = i;
+                allCells[count].xIndex = j;
+                count++;
+            }
+        }
+    }
+
+    public void CheckRow(int row, int direction)
+    {
+        int count = 0;
+        int i = (direction > 0) ? maxX : minX;
+        int condition = (direction > 0) ? minX : maxX;
+
+        while (count < 4)
+        {
+            if (tiles[row, i].transform.childCount != 0)
+            {
+                tiles[row, i].GetComponentInChildren<Tile>().MoveRight(row);
+            }
+            count++;
+
+            i = i > condition ? i-- : i++;
+        }
+
+    }
+
+    public void Combine(Tile a, Tile b)
+    {
+        //GameObject myNewTile = Instantiate(tileToSpawn, a.transform);
+        //myNewTile.GetComponent<Tile>().value = a.value * 2;
+    }
+}
